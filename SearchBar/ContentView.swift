c1c1991243ed4344f30287cfefebbd36948c72a9
strawberryhackers <pso2lovejc@gmail.com>
2019-12-18 03:17:37 +0900

    Initial Commit

---
+++ SearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBar
+//
+//  Created by 根本翔希 on 2019/12/18.
+//  Copyright © 2019 根本翔希. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
